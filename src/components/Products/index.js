f3881f3991fee5c66937925ec363e9986ddf9d39
VamsiK21 <vamsizxc19@gmail.com>
2023-03-21 05:42:43 +0000

    Redirect component used and withRoute component also used to navigate to desired component

---
+++ src/components/Products/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+
+import './index.css'
+
+const Products = () => (
+  <div className="products-container">
+    <img
+      className="products-img"
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+    />
+  </div>
+)
+
+export default Products
